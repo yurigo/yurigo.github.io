425b48da043cd1a3aae807e6f83c366bfff5f813
Yuri <ygonzoo@gmail.com>
2017-10-24 19:21:41 +0200

    improved the image aspect ratio

--- js/main.js
+++ js/main.js
@@ -1,7 +1,30 @@
 $(document).ready(
     function () {
+
+        adjustImage();
+
         var d = new Date();
         var n = d.getFullYear();
         $("#year").text(n);
     }
+
+
 );
+
+$(window).resize(
+    function () {
+        adjustImage();
+    }
+);
+
+function adjustImage() {
+    //$(".logo").removeClass("img-horizontal");
+    //$(".logo").removeClass("img-vertical");
+
+    if ($(window).width() > 650) {
+        $(".logo").addClass("img-horizontal");
+    }
+    else {
+        $(".logo").removeClass("img-horizontal");
+    }
+}
